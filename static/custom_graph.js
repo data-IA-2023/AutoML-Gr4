94e8eecab65d62296097d576178117e8599a10b6
AlexandreBoult <alex.boult@orange.fr>
2024-09-17 21:45:05 +0200

    graph display prototype v0.1

--- static/custom_graph.js
+++ static/custom_graph.js
@@ -9,38 +9,72 @@ const ctx = canvas.getContext('2d');
 function getRandomHexRGB() {
   const letters = '0123456789ABCDEF';
   let color = '#';
-  for (let i = 0; i < 6; i++) {
+  for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
 
-function euclideanNorm(arr) {
-    return Math.sqrt(arr.reduce((acc, val) => acc + val * val, 0));
-}
-
 function sumArray(arr) {
     let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
       sum += arr[i];
     }
     return sum;
 }
 
+function euclideanNorm(arr) {
+  const s_arr=[];
+  for (var i = 0; i < arr.length; i++) {
+    s_arr.push(arr[i]*arr[i]);
+  }
+  return Math.sqrt(sumArray(s_arr));
+}
+
 function termByTermSum(vec1, vec2) {
     const result = [];
-    for (let i = 0; i < vec1.length; i++) {
+    for (var i = 0; i < vec1.length; i++) {
       result.push(vec1[i] + vec2[i]);
     }
     return result;
 }
 
+function termByTermDiff(vec1, vec2) {
+  const result = [];
+  for (var i = 0; i < vec1.length; i++) {
+    result.push(vec1[i] - vec2[i]);
+  }
+  return result;
+}
+
 function termByTermMult(vec1, vec2) {
     const result = [];
-    for (let i = 0; i < vec1.length; i++) {
+    for (var i = 0; i < vec1.length; i++) {
       result.push(vec1[i] * vec2[i]);
     }
     return result;
+}
+
+function scalarProduct(vec1, vec2) {
+  return sumArray(termByTermMult(vec1,vec2));
+}
+
+function vectorProduct(number, vec) {
+  const result = [];
+  for (var i = 0; i < vec.length; i++) {
+    result.push(vec[i] * number);
+  }
+  return result
+}
+
+function removeElement(arr, element) {
+  const result = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] != element) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
 
 let isDragging = false;
@@ -50,13 +84,16 @@ let isDraggingSpace = false;
 let startX, startY;
 let x_pos, y_pos;
 let fixedPosArr = [];
+let contextBox = [-9999,-9999,-9999,-9999];
+let contextActions = [];
+let nodeToBeDeleted = -1;
 
-var nodes=[{pos:[10,10],inputs:[],outputs:[1,2],name:"node 1",color:getRandomHexRGB(),size:{x:200,y:100}},
-{pos:[250,10],inputs:[0],outputs:[],name:"node 2",color:getRandomHexRGB(),size:{x:200,y:100}},
-{pos:[250,210],inputs:[0],outputs:[3],name:"node 3",color:getRandomHexRGB(),size:{x:200,y:100}},
-{pos:[500,210],inputs:[2],outputs:[],name:"node 4",color:getRandomHexRGB(),size:{x:200,y:100}}
+var nodes=[{pos:[10,10],outputs:[1,2],name:"node 1",color:getRandomHexRGB(),size:{x:200,y:100}},
+{pos:[250,10],outputs:[],name:"node 2",color:getRandomHexRGB(),size:{x:200,y:100}},
+{pos:[250,210],outputs:[3],name:"node 3",color:getRandomHexRGB(),size:{x:200,y:100}},
+{pos:[500,210],outputs:[],name:"node 4",color:getRandomHexRGB(),size:{x:200,y:100}}
 ];
-var CurrentNode=0;
+var currentNode=0;
 let voidClick;
 
 for (var i = 0; i < nodes.length; i++) {
@@ -71,7 +108,7 @@ function drawRectangle(x, y, size_x, size_y, color) {
 
 function drawNode(x, y, name, color, content, size, id) {
   drawRectangle(x-2, y-2, size.x+4, size.y+4+20, 'black')
-  if (id === CurrentNode) {
+  if (id === currentNode) {
     drawRectangle(x-2, y-2, size.x+4, size.y+4+20, 'white')
   } else {
     drawRectangle(x-2, y-2, size.x+4, size.y+4+20, 'black')
@@ -81,15 +118,18 @@ function drawNode(x, y, name, color, content, size, id) {
   ctx.font = "bold 18px Arial";
   ctx.fillText(name, x+2, y+16)
   drawRectangle(x, y+20, size.x, size.y, 'grey')
-  drawRectangle(x+size.x-10, y+20+size.y-10, 10, 10, 'black')
+  drawRectangle(x+size.x-10, y+10+size.y, 10, 10, 'black')
+  drawRectangle(x-2, y+size.y/2+6, 10, 10, 'green')
+  drawRectangle(x+size.x-8, y+size.y/2+6, 10, 10, 'green')
 }
 
 function drawAllNodes() {
   //ctx.clearRect(0, 0, canvas.width, canvas.height);
+  contextBox = [-9999,-9999,-9999,-9999];
   ctx.fillStyle = "rgb(200, 200, 200)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   for (var i = 0; i < nodes.length; i++) {
-    if (i != CurrentNode) {
+    if (i != currentNode) {
         drawNode(nodes[i].pos[0], nodes[i].pos[1],nodes[i].name,nodes[i].color,"",nodes[i].size,i);
     }
     for (var j = 0; j < nodes[i].outputs.length; j++) {
@@ -100,14 +140,43 @@ function drawAllNodes() {
       ctx.lineTo(nodes[nodes[i].outputs[j]].pos[0], nodes[nodes[i].outputs[j]].pos[1]+nodes[nodes[i].outputs[j]].size.y/2+1+10); // end point
       ctx.stroke();
     }
-
   }
-  drawNode(nodes[CurrentNode].pos[0], nodes[CurrentNode].pos[1],nodes[CurrentNode].name,nodes[CurrentNode].color,"",nodes[CurrentNode].size,CurrentNode);
+  if (nodes.length > 0) {
+    drawNode(nodes[currentNode].pos[0], nodes[currentNode].pos[1],nodes[currentNode].name,nodes[currentNode].color,"",nodes[currentNode].size,currentNode);
+  }
 }
 
-function drawContext(x,y) {
-    drawRectangle(x-2, y-2, 154, 204, 'black')
-    drawRectangle(x, y, 150, 200, 'grey')
+function drawContext(x,y,options,width) {
+    drawRectangle(x-2, y-2, width+4, options.length*20+4, 'black')
+    for (var i = 0; i < options.length; i++) {
+      if ((i)%2 === 0){
+        drawRectangle(x, y+i*20, width, 20, 'grey')
+      } else {
+        drawRectangle(x, y+i*20, width, 20, "rgb(140, 140, 140)")
+      }
+      ctx.fillStyle = "rgb(255, 255, 255)";
+      ctx.font = "bold 18px Arial";
+      ctx.fillText(options[i], x+2, i*20+y+16);
+    }
+    contextBox = [x,x+width,y,y+options.length*20];
+}
+
+function deleteConection(conection) {
+  nodes[conection[0]].outputs=removeElement(nodes[conection[0]].outputs,conection[1]);
+}
+
+function deleteNode(node_index) {
+  nodeToBeDeleted=node_index;
+}
+
+function changeColor(node) {
+  nodes[node].color=getRandomHexRGB();
+}
+
+function newNode(name_and_pos) {
+  const name=name_and_pos.name;
+  var pos=name_and_pos.pos;
+  nodes.push({pos:pos,outputs:[],name:name,color:getRandomHexRGB(),size:{x:200,y:100}});
 }
 
 // Handle mouse events
@@ -115,44 +184,83 @@ canvas.addEventListener('mousedown', (event) => {
     startX = event.clientX - canvas.offsetLeft;
     startY = event.clientY - canvas.offsetTop;
     voidClick=true;
-    for (var i = 0; i < nodes.length; i++) {
-      x_pos=nodes[i].pos[0];
-      y_pos=nodes[i].pos[1];
-      fixedPosArr[i]=[startX-x_pos,startY-y_pos];
-      if (startX-x_pos >= 0 && startX-x_pos <= nodes[i].size.x && startY-y_pos >= 0 && startY-y_pos <= nodes[i].size.y+20) {
-        CurrentNode=i;
-        voidClick=false;
-        relativeX = startX-x_pos;
-        relativeY = startY-y_pos;
-        if (relativeY <= 20) {
-            isDragging = true;
-        }
-        if (relativeX >= nodes[CurrentNode].size.x-10 && relativeY >= nodes[CurrentNode].size.y-10) {
-            isResizing = true;
+    if (startX >= contextBox[0] && startX <= contextBox[1] && startY >= contextBox[2] && startY <= contextBox[3]) {
+      voidClick=false;
+      for (var i = 0; i < contextActions.length; i++) {
+        if (Math.floor((startY-contextBox[2])/20) === i) {
+          contextActions[i][0](contextActions[i][1])
         }
       }
-      for (var j = 0; j < nodes[i].outputs.length; j++) {
-        pos1=[nodes[i].pos[0]+nodes[i].size.x+2, nodes[i].pos[1]+nodes[i].size.y/2+1+10];
-        pos2=[nodes[nodes[i].outputs[j]].pos[0], nodes[nodes[i].outputs[j]].pos[1]+nodes[nodes[i].outputs[j]].size.y/2+1+10];
-        vec=termByTermSum(pos2,[-pos1[0],-pos1[1]]);
-        mouse_vec=termByTermSum([startX,startY],[-pos1[0],-pos1[1]]);
-        if (sumArray(termByTermMult(vec,mouse_vec))/euclideanNorm(vec)/euclideanNorm(mouse_vec)>0.9999 && startX<=pos2[0]) {
-            voidClick=false;
-            drawContext(startX,startY)
+      drawAllNodes()
+    } else {
+      contextBox = [-9999,-9999,-9999,-9999];
+      for (var i = 0; i < nodes.length; i++) {
+        x_pos=nodes[i].pos[0];
+        y_pos=nodes[i].pos[1];
+        fixedPosArr[i]=[startX-x_pos,startY-y_pos];
+        for (var j = 0; j < nodes[i].outputs.length; j++) {
+          pos1=[nodes[i].pos[0]+nodes[i].size.x+2, nodes[i].pos[1]+nodes[i].size.y/2+1+10];
+          pos2=[nodes[nodes[i].outputs[j]].pos[0], nodes[nodes[i].outputs[j]].pos[1]+nodes[nodes[i].outputs[j]].size.y/2+1+10];
+          vec=termByTermDiff(pos2,pos1);
+          mouse_vec1=termByTermDiff([startX,startY],pos1);
+          // mouse_vec2=termByTermDiff(pos2,[startX,startY]);
+          proj_vec=vectorProduct(scalarProduct(mouse_vec1,vec)/euclideanNorm(vec)/euclideanNorm(vec), vec);
+          ortho_vec=termByTermDiff(mouse_vec1,proj_vec);
+          const tol=5;
+          if (euclideanNorm(ortho_vec)<tol && (startY-tol <= Math.max(pos1[1],pos2[1])) && (startY+tol >= Math.min(pos1[1],pos2[1])) && (startX-tol <= Math.max(pos1[0],pos2[0])) && (startX+tol >= Math.min(pos1[0],pos2[0]))) {
+              voidClick=false;
+              drawAllNodes()
+              drawContext(startX,startY,["🗑️ Delete conection"],200);
+              let i_copy=JSON.parse(JSON.stringify(i));
+              let j_copy=JSON.parse(JSON.stringify(nodes[i].outputs[j]));
+              conection=[i_copy,j_copy];
+              contextActions = [[deleteConection,conection]];
+          }
+        }
+        if (startX-x_pos >= 0 && startX-x_pos <= nodes[i].size.x && startY-y_pos >= 0 && startY-y_pos <= nodes[i].size.y+20) {
+          currentNode=i;
+          voidClick=false;
+          relativeX = startX-x_pos;
+          relativeY = startY-y_pos;
+          drawAllNodes()
+          if (relativeY <= 20) {
+              isDragging = true;
+              drawContext(startX,startY,["🎨 Change color","🗑️ Delete node"],160);
+              contextActions = [[changeColor,currentNode],[deleteNode,currentNode]];
+          } else if (relativeX >= nodes[currentNode].size.x-10 && relativeY >= nodes[currentNode].size.y-10) {
+              isResizing = true;
+          }
         }
       }
     }
     if (voidClick) {
-        // drawContext(0,0)
         isDraggingSpace=true;
         drawAllNodes()
+        drawContext(startX,startY,["➕ Add new node"],160);
+        contextActions = [[newNode,{name:"new node",pos:[startX,startY]}]];
     }
+    if (nodeToBeDeleted>-1) {
+      nodes.splice(nodeToBeDeleted,1);
+      for (var i = 0; i < nodes.length; i++) {
+        nodes[i].outputs=removeElement(nodes[i].outputs,nodeToBeDeleted);
+      }
+      for (var i = 0; i < nodes.length; i++) {
+        for (var j = 0; j < nodes[i].outputs.length; j++) {
+          if (nodes[i].outputs[j] > nodeToBeDeleted) {
+            nodes[i].outputs[j]=nodes[i].outputs[j]-1;
+          }
+        }
+      }
+      nodeToBeDeleted=-1;
+      currentNode=0;
+      drawAllNodes()
+    };
 });
 
 canvas.addEventListener('mousemove', (event) => {
     if (isDragging) {
-        nodes[CurrentNode].pos[0] = event.clientX - canvas.offsetLeft - fixedPosArr[CurrentNode][0];
-        nodes[CurrentNode].pos[1] = event.clientY - canvas.offsetTop - fixedPosArr[CurrentNode][1];
+        nodes[currentNode].pos[0] = event.clientX - canvas.offsetLeft - fixedPosArr[currentNode][0];
+        nodes[currentNode].pos[1] = event.clientY - canvas.offsetTop - fixedPosArr[currentNode][1];
         drawAllNodes()
     }
     if (isDraggingSpace) {
@@ -163,13 +271,13 @@ canvas.addEventListener('mousemove', (event) => {
         drawAllNodes()
     }
     if (isResizing) {
-        nodes[CurrentNode].size.x = event.clientX - canvas.offsetLeft-nodes[CurrentNode].pos[0];
-        nodes[CurrentNode].size.y = event.clientY - canvas.offsetTop-nodes[CurrentNode].pos[1]-20;
-        if (nodes[CurrentNode].size.x < 80) {
-            nodes[CurrentNode].size.x=80
+        nodes[currentNode].size.x = event.clientX - canvas.offsetLeft-nodes[currentNode].pos[0];
+        nodes[currentNode].size.y = event.clientY - canvas.offsetTop-nodes[currentNode].pos[1]-20;
+        if (nodes[currentNode].size.x < 80) {
+            nodes[currentNode].size.x=80
         }
-        if (nodes[CurrentNode].size.y < 50) {
-            nodes[CurrentNode].size.y=50
+        if (nodes[currentNode].size.y < 50) {
+            nodes[currentNode].size.y=50
         }
         drawAllNodes()
     }
